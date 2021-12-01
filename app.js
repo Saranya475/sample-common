f4d23ea9562c962c40f5271c8359e0b60a7c9561
Saranya475 <sarasaran1993@gmail.com>
2021-12-01 22:59:38 +0530

    sample-common added

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+
+
+app.get("/login",function(req,res)
+{
+res.send("login api");
+});
+
+app.get("/memberlist",function(req,res)
+{
+res.send("memberlist api");
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
