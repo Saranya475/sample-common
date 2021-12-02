cb18265b4ee72cc717210c1449b648d70cac2a5f
Saranya475 <sarasaran1993@gmail.com>
2021-12-02 16:19:29 +0530

    changes done

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 const router = require('express').Router();
-
+const REDIS_PORT = Number(process.env.REDIS_PORT);
+const REDIS_HOST= process.env.REDIS_HOST
+console.log("**************printing export things*******************");
+console.log(REDIS_PORT);
+console.log(REDIS_HOST);
 router.get("/login",function(req,res)
 {
 res.send("login api");
