61716e5e14a04fb6320b31e0d368fa83a34049e7
Saranya475 <sarasaran1993@gmail.com>
2021-12-02 15:03:15 +0530

    changes done

--- app.js
+++ app.js
@@ -4,41 +4,17 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+const router = require('express').Router();
 
-
-app.get("/login",function(req,res)
+router.get("/login",function(req,res)
 {
-  console.log("login api called");
 res.send("login api");
 });
 
-app.get("/memberlist",function(req,res)
+router.get("/memberlist",function(req,res)
 {
 res.send("memberlist api");
 });
-// module.exports=
-// }
 
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-//  });
-// var login = function (req, res) {
-//   return req.body;
-// }
-// var memberlist = function (req, res) {
-//   return "memberlist api";
-// }
-// module.exports={
-//   login,
-//   memberlist
-// }
-// module.exports = class Square {
-//   constructor(width) {
-//     this.width = width;
-//   }
-
-//   area() {
-//     return this.width ** 2;
-//   }
-// };
+module.exports = router;
 
